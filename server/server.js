59c5886f8079089a42f32040c0c4e86c73aa5666
Gary Lau <kitgarycode@gmail.com>
2016-08-06 14:09:30 -0700

    testing

--- server/server.js
+++ server/server.js
@@ -7,24 +7,28 @@ var staticRoot = __dirname + '/';
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(staticRoot));
+//app.use(express.static(staticRoot));
 
-app.use(function(req, res, next) {
+//app.use(function(req, res, next) {
 
-    // if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if(accept !== 'html') {
-        return next();
-    }
+    //// if the request is not html then move along
+    //var accept = req.accepts('html', 'json', 'xml');
+    //if(accept !== 'html') {
+        //return next();
+    //}
 
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== ''){
-        return next();
-    }
+    //// if the request has a '.' assume that it's for a file, move along
+    //var ext = path.extname(req.path);
+    //if (ext !== ''){
+        //return next();
+    //}
 
-    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+    //fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
+//});
+
+app.get('/port', function(req, res) {
+    res.send('The port used is ' + process.env.PORT);
 });
 
 app.get('/user', function(req, res) {
