152c786a250c03a98690d355e6b37f8d95653efb
Gary Lau <kitgarycode@gmail.com>
2016-08-06 14:35:46 -0700

    change order

--- build/server.js
+++ build/server.js
@@ -7,6 +7,16 @@ var staticRoot = __dirname + '/';
 
 app.set('port', (process.env.PORT || 3000));
 
+app.get('/port', function(req, res) {
+    res.send('The port used is ' + process.env.PORT);
+});
+
+app.get('/user', function(req, res) {
+    res.send('Got a Get request at /user');
+});
+
+app.use(express.static(staticRoot));
+
 app.use(function(req, res, next) {
 
     // if the request is not html then move along
@@ -24,16 +34,6 @@ app.use(function(req, res, next) {
     fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
 });
-
-app.get('/port', function(req, res) {
-    res.send('The port used is ' + process.env.PORT);
-});
-
-app.get('/user', function(req, res) {
-    res.send('Got a Get request at /user');
-});
-
-app.use(express.static(staticRoot));
 
 app.listen(app.get('port'), function() {
     console.log('app running on port', app.get('port'));
